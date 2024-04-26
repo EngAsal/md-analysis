1c804de112e02de942b46b6f724497feace7f3de
Asal Azar <yabiasal@gmail.com>
2024-04-26 16:53:23 +0100

    compare chain rmsds

--- analysis.py
+++ analysis.py
@@ -21,8 +21,8 @@ except Exception as e:
     print(f"Error occurred while changing the working directory: {e}")
 # %%
 ### Load the universe and calculate rmsd
-u = mda.Universe('data/ABC_newbox.gro', 'cluster_aligned.xtc')
-ref = mda.Universe('data/ABC_newbox.gro', 'cluster_aligned.xtc')
+u = mda.Universe('data/ABC_5ue6_protein.pdb', 'cluster_aligned.xtc')
+ref = mda.Universe('data/ABC_5ue6_protein.pdb', 'cluster_aligned.xtc')
 
 u.trajectory[-1]  # set mobile trajectory to last frame
 ref.trajectory[0]  # set reference trajectory to first frame
@@ -37,8 +37,8 @@ aligner = align.AlignTraj(u, ref, select='protein',
                            filename='final_3_aligned.xtc').run()
 #%%
 # Load the aligned trajectory and calculate rmsd
-u_aligned = mda.Universe('chainC.gro', 'chainC_cluster_aligned.xtc')
-ref_aligned = mda.Universe('chainC.gro', 'chainC_cluster_aligned.xtc')
+u_aligned = mda.Universe('data/ABC_newbox.gro', 'cluster_aligned.xtc')
+ref_aligned = mda.Universe('data/ABC_newbox.gro', 'cluster_aligned.xtc')
 
 u_aligned.trajectory[-1]  # set mobile trajectory to last frame
 ref_aligned.trajectory[0]  # set reference trajectory to first frame
@@ -54,20 +54,59 @@ print(f"Aligned RMSD: {aligned_rmsd:.2f}")
 R = MDAnalysis.analysis.rms.RMSD(u_aligned, ref_aligned, select="backbone")
 R.run()
 rmsd = R.rmsd.T
-df = pd.DataFrame(R.rmsd, columns=['Frame', 'Time (ps)', 'Backbone'])
+df = pd.DataFrame(R.rmsd, columns=['Frame', r'Time ($\mu$s)', 'Backbone'])
+df[r'Time ($\mu$s)'] = df[r'Time ($\mu$s)'] / 1000000
 #%%
 # RMSD plot 
-ax = df.plot(x='Time (ps)', y=['Backbone'], kind='line')
+ax = df.plot(x=r'Time ($\mu$s)', y=['Backbone'], kind='line', legend=False, color='black')
 ax.set_ylabel(r'RMSD ($\AA$)')
-ax.set_title("RMSD of chain C")
+#%%
+# Plot rmsd of all chains in one plot
+u_A = mda.Universe('chainA.gro', 'chainA_cluster_aligned.xtc')
+ref_A = mda.Universe('chainA.gro', 'chainA_cluster_aligned.xtc')
+u_A.trajectory[-1]  # set mobile trajectory to last frame
+ref_A.trajectory[0]  # set reference trajectory to first frame
+R_A = MDAnalysis.analysis.rms.RMSD(u_A, ref_A, select="backbone")
+R_A.run()
+rmsd_A = R_A.rmsd.T
+
+u_B = mda.Universe('chainB.gro', 'chainB_cluster_aligned.xtc')
+ref_B  = mda.Universe('chainB.gro', 'chainB_cluster_aligned.xtc')
+u_B.trajectory[-1]  # set mobile trajectory to last frame
+ref_B.trajectory[0]  # set reference trajectory to first frame
+R_B = MDAnalysis.analysis.rms.RMSD(u_B, ref_B, select="backbone")
+R_B.run()
+rmsd_B = R_B.rmsd.T
+
+u_C = mda.Universe('chainC.gro', 'chainC_cluster_aligned.xtc')
+ref_C = mda.Universe('chainC.gro', 'chainC_cluster_aligned.xtc')
+u_C.trajectory[-1]  # set mobile trajectory to last frame
+ref_C.trajectory[0]  # set reference trajectory to first frame
+R_C = MDAnalysis.analysis.rms.RMSD(u_C, ref_C, select="backbone")
+R_C.run()
+rmsd_C = R_C.rmsd.T
+
+#%%
+df_A = pd.DataFrame(R_A.rmsd, columns=['Frame', r'Time ($\mu$s)', 'Backbone'])
+df_B = pd.DataFrame(R_B.rmsd, columns=['Frame', r'Time ($\mu$s)', 'Backbone'])
+df_C = pd.DataFrame(R_C.rmsd, columns=['Frame', r'Time ($\mu$s)', 'Backbone'])
+df_A[r'Time ($\mu$s)'] = df_A[r'Time ($\mu$s)'] / 1000000
+df_B[r'Time ($\mu$s)'] = df_B[r'Time ($\mu$s)'] / 1000000
+df_C[r'Time ($\mu$s)'] = df_C[r'Time ($\mu$s)'] / 1000000
+# RMSD plot 
+plt.plot(df_A[r'Time ($\mu$s)'], df_A['Backbone'], color = 'darkcyan', alpha = 0.9, label = 'chain A')
+plt.plot(df_B[r'Time ($\mu$s)'], df_B['Backbone'],  color = 'yellowgreen',  alpha = 0.9, label = 'chain B')
+plt.plot(df_C[r'Time ($\mu$s)'], df_C['Backbone'],  color = 'chocolate',  alpha = 0.9, label = 'chain C')
+plt.ylabel(r'RMSD ($\AA$)')
+plt.legend()
+
 # %%
 # Calculate RMSF
-c_alphas = u_aligned.select_atoms('protein and name CA')
+c_alphas = u_aligned.select_atoms('protein and backbone')
 R = rms.RMSF(c_alphas).run()
 #%%
 # RMSF plot
-plt.plot(c_alphas.resids, R.results.rmsf)
-plt.title("RMSF of the monomer")
+plt.plot(c_alphas.resids, R.results.rmsf, color='purple')
 plt.xlabel('Residue number')
 plt.ylabel('RMSF ($\AA$)')
 # %%
@@ -81,9 +120,8 @@ selected_resids = c_alphas.resids[indices]
 selected_rmsf = R.results.rmsf[indices]
 
 # Plot the RMSF for the selected range of residues
-plt.plot(selected_resids, selected_rmsf)
+plt.plot(selected_resids, selected_rmsf, color='purple')
 plt.xlabel('Residue number')
-plt.title("RMSF of the monomer, residues 55 to 349")
 plt.ylabel('RMSF ($\AA$)')
 plt.show()
 # %%
@@ -215,4 +253,61 @@ else:
 
 #%%
 u = mda.Universe('data/ABC.pdb')
+# %%
+path = '/home/pghw87/Documents/md-sim/5ue6/monomer/monomer'
+os.chdir(path)
+# %%
+u_aligned = mda.Universe('5ue6_newbox.gro', 'final_3000_aligned.xtc')
+# %%
+matrix = diffusionmap.DistanceMatrix(u_aligned, select='name CA').run()
+#%%
+start_frame = 0
+step = 100
+
+# Using the context manager to handle trajectory slicing
+
+    # Now compute the distance matrix only for the sliced trajectory
+for ts in u_aligned.trajectory[::step]:
+    matrix = diffusionmap.DistanceMatrix(u_aligned, select='name CA').run()
+
+    # Visualize the results
+    plt.imshow(matrix.dist_matrix, cmap='viridis')
+    plt.xlabel('Frame')
+    plt.ylabel('Frame')
+    plt.colorbar(label=r'RMSD ($\AA$)')
+    plt.show()
+# %%
+def write_sliced_trajectory(universe, slice_step, output_filename):
+    # Initialize the trajectory writer with the output filename and the number of atoms
+    with XTCWriter(output_filename, n_atoms=universe.atoms.n_atoms) as writer:
+        # Iterate over the sliced trajectory
+        for ts in universe.trajectory[::slice_step]:
+            writer.write(universe.atoms)  # Write the current frame to the file
+
+# %%
+write_sliced_trajectory(u_aligned, 100, 'final_3000_100sliced.xtc')
+# %%
+u_sliced = mda.Universe('5ue6_newbox.gro', 'final_3000_100sliced.xtc')
+# %%
+matrix = diffusionmap.DistanceMatrix(u_sliced, select='name CA').run()
+# %%
+plt.imshow(matrix.dist_matrix, cmap='viridis')
+plt.xlabel('Frame')
+plt.ylabel('Frame')
+plt.colorbar(label=r'RMSD ($\AA$)')
+# %%
+time_step = 20 # Time step in picoseconds per frame
+start_frame = 0  # Starting frame
+step = 100  # Step for slicing
+num_frames = len(u_aligned.trajectory)  # Total number of frames in the trajectory
+
+ 
+total_time = num_frames * step * 20 / 100000000
+plt.imshow(matrix.dist_matrix, cmap='viridis', extent = [0,total_time, 0, total_time])
+
+# Assuming 'matrix.dist_matrix' is your distance matrix calculated as previously described
+plt.xlabel(r'Time ($\mu$s)')
+plt.ylabel(r'Time ($\mu$s)')
+plt.colorbar(label=r'RMSD ($\AA$)')
+plt.show()
 # %%
